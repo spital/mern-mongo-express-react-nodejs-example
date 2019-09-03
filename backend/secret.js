d8489910632f5c9ebffbd75b69fb81b5efb49c1d
Jiri Spitalsky (ext DCIM) <jura@spital.cz>
2019-09-03 21:18:42 +0200

    mongodb read write not working

--- backend/secret.js
+++ backend/secret.js
@@ -1,5 +1,6 @@
 const secrets = {
-  dbUri: "mongodb://jelo:a9bc839993@ds151382.mlab.com:51382/jelotest"
+  // dbUri: "mongodb://jelo:a9bc839993@ds151382.mlab.com:51382/jelotest"
+  dbUri: "mongodb://localhost:27017/mydb"
 };
 
 const getSecret = key => secrets[key];
